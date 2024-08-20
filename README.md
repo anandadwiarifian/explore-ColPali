869a51c1c9d83a35584b2bf326e8f602d8cc5a4e
ananda.dwi <arifian.anandadwi@gmail.com>
2024-08-20 09:56:26 +0700

    update

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # explore-ColPali
+
+## Prerequisite
+### 1. Prepare HuggingFace Hub Token
+0. Generate HuggingFace Hub Token if you dont already have it
+1. Create a `.env` file in the project dir and put the token in `HUGGING_FACE_HUB_TOKEN` field's value
+```env
+HUGGING_FACE_HUB_TOKEN={{TOKEN}}
+```
+
+### 2. Request Access to `paligemma-3b-mix-448` model [here](https://huggingface.co/google/paligemma-3b-mix-448)
+
+
+### 3. Install poppler
+For ubuntu, run: 
+```sh
+sudo apt-get install poppler-utils
+```
+For macOS, run:
+```sh
+brew install poppler
+```
+Make sure that the poppler binary is added to the PATH.
+
+### 4. Install Python Libraries
+```sh
+pip install -r requirements.txt
+```
--- colpali.py
+++ colpali.py
@@ -19,7 +19,7 @@ class ColPaliModel:
     def __init__(self) -> None:
         self.model_name = "vidore/colpali"
         self.model = ColPali.from_pretrained(
-            "google/paligemma-3b-mix-448", torch_dtype=torch.bfloat16, device_map="cuda").eval()
+            "google/paligemma-3b-mix-448", torch_dtype=torch.bfloat16, device_map="cpu").eval()
         self.model.load_adapter(self.model_name)
         self.processor = AutoProcessor.from_pretrained(self.model_name)
         self.ds = []
@@ -71,5 +71,42 @@ class ColPaliModel:
             qs.extend(list(torch.unbind(embeddings_query.to("cpu"))))
 
         # run evaluation
-        retriever_evaluator = CustomEvaluator(is_multi_vector=True)
-        return retriever_evaluator.evaluate(qs, self.ds)
+        retriever_evaluator = Evaluator(is_multi_vector=True)
+        return retriever_evaluator.evaluate(qs, self.ds, "cpu")
+
+class Evaluator(CustomEvaluator):
+    def evaluate_colbert(self, qs, ps, device_map, batch_size=128) -> torch.Tensor:
+        scores = []
+        for i in range(0, len(qs), batch_size):
+            scores_batch = []
+            qs_batch = torch.nn.utils.rnn.pad_sequence(qs[i : i + batch_size], batch_first=True, padding_value=0).to(
+                device_map
+            )
+            for j in range(0, len(ps), batch_size):
+                ps_batch = torch.nn.utils.rnn.pad_sequence(
+                    ps[j : j + batch_size], batch_first=True, padding_value=0
+                ).to(device_map)
+                scores_batch.append(torch.einsum("bnd,csd->bcns", qs_batch, ps_batch).max(dim=3)[0].sum(dim=2))
+            scores_batch = torch.cat(scores_batch, dim=1).cpu()
+            scores.append(scores_batch)
+        scores = torch.cat(scores, dim=0)
+        return scores
+
+    def evaluate(self, qs, ps, device_map: str):
+        if self.is_multi_vector:
+            scores = self.evaluate_colbert(qs, ps, device_map)
+        else:
+            scores = self.evaluate_biencoder(qs, ps)
+
+        assert scores.shape[0] == len(qs)
+
+        arg_score = scores.argmax(dim=1)
+        # compare to arange
+        accuracy = (arg_score == torch.arange(scores.shape[0], device=scores.device)).sum().item() / scores.shape[0]
+        print(arg_score)
+        print(f"Top 1 Accuracy (verif): {accuracy}")
+
+        # cast to numpy
+        # scores = scores.cpu().numpy()
+        scores = scores.to(torch.float32).cpu().numpy()
+        return scores
